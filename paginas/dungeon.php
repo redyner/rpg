2d19ad3b3a14a23dee1980e35cc6a3a1de19cdef
Rediner <rediner.vinhal@cmtecnologia.com.br>
2021-09-09 18:37:01 -0300

    novos inimigos adicionados

--- paginas/dungeon.php
+++ paginas/dungeon.php
@@ -25,8 +25,11 @@
         </div>
         <div id="selecao_inimigo">
         <a href="?pagina=batalha&id=1" ><span class="avatar_inimigo_selecao" id="Javali"></span></a>
+        <a href="?pagina=batalha&id=5" ><div class="avatar_inimigo_selecao" id="Goblin"></div></a>
         <a href="?pagina=batalha&id=2" ><div class="avatar_inimigo_selecao" id="Urso"></div></a>
+        <a href="?pagina=batalha&id=6" ><div class="avatar_inimigo_selecao" id="Orc"></div></a>
         <a href="?pagina=batalha&id=3" ><div class="avatar_inimigo_selecao" id="Lobo"></div></a>
         <a href="?pagina=batalha&id=4" ><div class="avatar_inimigo_selecao" id="Drag"></div></a>
+        <a href="?pagina=batalha&id=7" ><div class="avatar_inimigo_selecao" id="Boss"></div></a>
         </div>
          </div>
