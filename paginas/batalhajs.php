10b72855b67d1b70c5599c5882bac70b844b15cb
Rediner <rediner.vinhal@cmtecnologia.com.br>
2021-09-13 14:59:06 -0300

    animações batalhajs

--- paginas/batalhajs.php
+++ paginas/batalhajs.php
@@ -67,7 +67,12 @@
 
 <script>
 
-document.getElementById("atacar").addEventListener("click",function(event){
+var sta_inimigo = <?php echo $atributos_inimigo['sta'] ?>;
+var sta_batalha_inimigo = sta_inimigo
+var sta_batalha_personagem = <?php echo $_SESSION['sta'] ?>;
+
+
+document.getElementById("atacar").addEventListener("click",setInterval(function(event){
     var sta = <?php echo $_SESSION['sta'] ?>;
     var str = <?php echo $_SESSION['str'] ?>;
     var int = <?php echo $_SESSION['int'] ?>;
@@ -76,11 +81,19 @@ document.getElementById("atacar").addEventListener("click",function(event){
     var hp_2 = document.getElementById("hp_atual_2");
     var teste = Math.floor(Math.random()*100+1);
     combate(sta,str,int,dex,hp_1,hp_2,teste)
-},)
+},3000))
 
 function combate(sta,str,int,dex,hp_1,hp_2,teste){
-    hp_1.style.width = teste+"%";
-    hp_2.style.width = teste+"%";
+ 
+    sta_batalha_inimigo -= str
+    var porcentagem_sta_inimigo = sta_batalha_inimigo*100/sta_inimigo;
+    var porcentagem_sta_personagem = sta_batalha_personagem*100/sta;
+    if (porcentagem_sta_inimigo<0) porcentagem_sta_inimigo = 0;
+    hp_1.style.width = porcentagem_sta_personagem+"%";
+    hp_2.style.width = porcentagem_sta_inimigo+"%";
+    if (porcentagem_sta_inimigo==0) clearInterval(combate)
+
+
 }
 
 </script>
