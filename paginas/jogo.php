5808f8875785458a6992a1ec1161ae0bbf226bb8
Samuel <samuel_oliver16@hotmail.com>
2021-09-23 21:41:57 -0300

    correção código sql inventario

--- paginas/jogo.php
+++ paginas/jogo.php
@@ -46,49 +46,46 @@ if ($porcentagem_xp < 0) $porcentagem_xp = 0;
 
     </div>
     <div id="selecao_item">
-
+        <div id='texto_inventario'>Inventario</div>
 
         <?php
         $id_personagem = $_SESSION['id_personagem'];
 
-        $sql = "SELECT i.slot, i.refino, i.id_item, i.id_personagem, it.id_item, it.nm_item, 
+        $sql = mysqli_query($conexao, "SELECT i.slot, i.refino, i.id_item, i.id_personagem, it.id_item, it.nm_item, 
             it.lv, a.sta, a.str, a.int, a.dex, a.id_item
             FROM rpg.inventarios i 
             JOIN rpg.itens it ON i.id_item = it.id_item
             JOIN rpg.atributos a ON a.id_item = i.id_item
-            WHERE i.id_personagem = $id_personagem";
-        $quantidade_item = mysqli_num_rows(mysqli_query($conexao, $sql));
-        $item_inventario = mysqli_fetch_assoc(mysqli_query($conexao, $sql));
-        
-        $slot = $item_inventario['slot'];
-        $id_item = $item_inventario['id_item'];
-        $nm_item = $item_inventario['nm_item'];
-        $lv = $item_inventario['lv'];
-        $sta = $item_inventario['sta'];
-        $str = $item_inventario['str'];
-        $int = $item_inventario['int'];
-        $dex = $item_inventario['dex'];
-        
-        ?>
-        <div id='texto_inventario'>Inventario</div>
-        <?php
-        for ($i = 0; $i < $quantidade_item; $i++) { ?>
+            WHERE i.id_personagem = $id_personagem");
+        while ($item_inventario = mysqli_fetch_assoc($sql)) {
 
-            <div id="slot" onclick="informacoes_item()" value="<?php echo $id_item ?>" class='slot'>
+            $slot = $item_inventario['slot'];
+            $id_item = $item_inventario['id_item'];
+            $nm_item = $item_inventario['nm_item'];
+            $lv = $item_inventario['lv'];
+            $sta = $item_inventario['sta'];
+            $str = $item_inventario['str'];
+            $int = $item_inventario['int'];
+            $dex = $item_inventario['dex'];
+        ?>
+            <div id="slot" onclick="informacoes_item()" value="<?php echo $id_item ?>" class='slot'>               
+            </div>
         <?php } ?>
 
-    </div>
-</div>
 
+    </div>
+    <?php
+
+    ?>
     <div id="informacoes_item">
         <p><?php echo $nm_item ?></p>
         <p>LEVEL - <?php echo $lv ?></p>
         <p>STA - <?php echo $sta ?></p>
         <p>STR - <?php echo $str ?></p>
         <p>INT - <?php echo $int ?></p>
-        <p>DEX - <?php echo $dex ?></p>   
+        <p>DEX - <?php echo $dex ?></p>
     </div>
-        <script>
-           // for (var i = 1; i <= 18; i++)
-               // document.write("<div onClick='informacoes_item();' id = 'slot"+i+"' class='slot' ></div>")
-        </script>
+    <script>
+        // for (var i = 1; i <= 18; i++)
+        // document.write("<div onClick='informacoes_item();' id = 'slot"+i+"' class='slot' ></div>")
+    </script>
