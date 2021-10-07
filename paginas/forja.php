7c2970837877ef21bb5edcfdfc62cbaab0cac2c1
Rediner <rediner.vinhal@cmtecnologia.com.br>
2021-10-06 23:37:39 -0300

    Ajuste de busca dos itens da Forja

--- paginas/forja.php
+++ paginas/forja.php
@@ -32,10 +32,8 @@ if ($porcentagem_xp < 0) $porcentagem_xp = 0;
                 FROM rpg.inventarios iv
                     JOIN rpg.itens i ON i.id_item = iv.id_item 
                     JOIN rpg.atributos a ON i.id_item = a.id_item
-                    JOIN rpg.classes c ON c.id_classe = i.id_classe
-                    JOIN rpg.personagens p ON p.id_classe = c.id_classe
+                    JOIN rpg.personagens p ON p.id_personagem = iv.id_personagem
                 WHERE p.id_personagem = '{$_SESSION['id_personagem']}'
-                    AND iv.slot IS NOT NULL
                     ORDER BY iv.slot";
         $resultado = mysqli_query($conexao,$sql);
         while ($info_item = mysqli_fetch_assoc($resultado))
@@ -60,7 +58,7 @@ if ($porcentagem_xp < 0) $porcentagem_xp = 0;
         for($i=0;$i<=14;$i++){
     ?>
 
-        <div id = 'slotf<?php if (isset($slot[$i])) echo $i ?>' name = '<?php if (isset($slot[$i])) echo $slot[$i]['nm_item'] ?>'  class='slot' ></div>
+        <div id = 'sloti<?php if (isset($slot[$i])) echo $i ?>' name = '<?php if (isset($slot[$i])) echo $slot[$i]['nm_item'] ?>'  class='slot'  <?php if (isset($slot[$i]['equipado']) && $slot[$i]['equipado'] == 'S') echo "style='border: 5px solid red'" ?> ></div>
     
     <?php
         }
@@ -72,23 +70,20 @@ if ($porcentagem_xp < 0) $porcentagem_xp = 0;
         for($i=0;$i<=14;$i++){
     ?>
     
-    <div id="info_slotf<?php echo $i ?>" class="informacoes_item">
+    <div id="info_sloti<?php echo $i ?>" class="informacoes_item">
         <p><?php echo isset($slot[$i]) ? $slot[$i]['nm_item'] : 0?></p>
         <p>LEVEL - <?php echo isset($slot[$i]) ? $slot[$i]['lv'] : 0 ?></p>
         <p>STA - <?php echo isset($slot[$i]) ? $slot[$i]['sta'] : 0 ?></p>
         <p>STR - <?php echo isset($slot[$i]) ? $slot[$i]['str'] : 0 ?></p>
         <p>INT - <?php echo isset($slot[$i]) ? $slot[$i]['int'] : 0 ?></p>
-        <p>DEX - <?php echo isset($slot[$i]) ? $slot[$i]['dex'] : 0 ?></p> 
-        <p>REF - <?php echo isset($slot[$i]) ? $slot[$i]['ref'] : 0 ?></p>   
-        <p>VALOR - <?php echo isset($slot[$i]) ? $slot[$i]['valor'] : 0 ?></p>    
+        <p>DEX - <?php echo isset($slot[$i]) ? $slot[$i]['dex'] : 0 ?></p>   
+        <p>REF - <?php echo isset($slot[$i]) ? $slot[$i]['ref'] : 0 ?></p>    
+        <p>VALOR - <?php echo isset($slot[$i]) ? $slot[$i]['valor'] : 0 ?></p>  
     </div>
 
     <?php
         }
     ?>
-
-
-
 
     <script>
 
@@ -102,6 +97,5 @@ if ($porcentagem_xp < 0) $porcentagem_xp = 0;
             ?>   
             [<?php echo isset($slot[15]) ? $slot[15]['id_inventario'] : 0 ?>,"<?php echo isset($slot[15]) ? $slot[15]['equipado'] : 0 ?>"]
         ]
-
 
     </script>
