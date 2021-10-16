138b751d0e1ae119a31cb888b3ecf681069d4121
Rediner <rediner.vinhal@cmtecnologia.com.br>
2021-10-16 12:42:29 -0300

    Att gold batalha

--- js/script.js
+++ js/script.js
@@ -78,6 +78,7 @@ function combate(hp_1, hp_2) {
         int_personagem += int_lv;
         dex_personagem += dex_lv;
         gold += lv_inimigo * 10;
+        $('.gold').html("GOLD - "+gold);
         document.getElementById("relatorio").innerHTML += "Parabens! voce subiu para o nivel " + lv + "<br>";
         ////////////////////////////////////////////////////////////////////////////////////////////
         //Aqui eu preciso atualizar as informações do banco de dados e atualizá-los na sessão php//
@@ -106,6 +107,7 @@ function combate(hp_1, hp_2) {
         });
       } else {
         gold += lv_inimigo * 10;
+        $('.gold').html("GOLD - "+gold);
         ////////////////////////////////////////////////////////////////////////////////////////////
         //Aqui eu preciso atualizar as informações do banco de dados e atualizá-los na sessão php//
         //////////////////////////////////////////////////////////////////////////////////////////
